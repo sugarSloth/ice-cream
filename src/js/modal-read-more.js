ce395ed65e0cc9295d42467cc53108d8948f5697
Iryna Sinkevych <45698707+sugarSloth@users.noreply.github.com>
2022-12-25 03:58:22 +0200

    Add some more
    Correct JS, some styles, changes to .html
    Not finished

---
+++ src/js/modal-read-more.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-read-more-open]'),
+    closeModalBtn: document.querySelector('[data-modal-read-more-close]'),
+    modal: document.querySelector('[data-modal-read-more]'),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
